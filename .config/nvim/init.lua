cfa2fd1b5497f4e1c479a32c754555b94820a7d8
iAverages <me@danielraybone.com>
2024-07-22 18:32:14 +0100

    izanami: 90 current  2024-07-22 18:32:10  24.11.20240712.7e7c39e  6.8.12                                  *

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,3 @@
 require("dan.config.lazy")
 require("dan.config.keymaps")
+require("dan.config.options")
--- nixos/packages.nix
+++ nixos/packages.nix
@@ -63,6 +63,7 @@
     postman
     unzip
     ripgrep
+    luarocks
   ];
 
   environment.variables = {
