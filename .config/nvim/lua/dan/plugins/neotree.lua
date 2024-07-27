506d2c5228a28978e6c04ea8ec65a610f58a4f8d
iAverages <me@danielraybone.com>
2024-07-27 20:48:20 +0100

    Fix files being shown in telescope and neotree

--- .config/nvim/lua/dan/plugins/neotree.lua
+++ .config/nvim/lua/dan/plugins/neotree.lua
@@ -1,28 +1,29 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-		"3rd/image.nvim",
-	},
-	opts = {
-		buffers = {
-			follow_currnet_file = {
-				enable = true
-			}
-		},
-		filesystem = {
-			visible = true,
-			hide_dotfiles = false,
-			hide_gitignore = false,
-			hide_by_name = {
-				"node_modules",
-				".git",
-				"target",
-				"vendor",
-			}
-		}
-	}
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+        "3rd/image.nvim",
+    },
+    opts = {
+        buffers = {
+            follow_currnet_file = {
+                enable = true,
+            },
+        },
+        filesystem = {
+            filtered_items = {
+                hide_dotfiles = false,
+                hide_gitignore = false,
+                hide_by_name = {
+                    "node_modules",
+                    ".git",
+                    "target",
+                    "vendor",
+                },
+            },
+        },
+    },
 }
