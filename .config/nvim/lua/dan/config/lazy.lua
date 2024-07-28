85d51915fe659c9fc9691048d7ad0c4fb50309de
iAverages <me@danielraybone.com>
2024-07-29 00:08:58 +0100

    lazy load by default + disable some builtins

--- .config/nvim/lua/dan/config/lazy.lua
+++ .config/nvim/lua/dan/config/lazy.lua
@@ -1,24 +1,24 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out,                            "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Use a protected call so we don't error out on first use
 local ok, lazy = pcall(require, "lazy")
 if not ok then
-	return
+    return
 end
 
 -- Make sure to setup `mapleader` and `maplocalleader` before
@@ -29,18 +29,31 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-	spec = {
-		--{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
-		{ import = "dan.plugins" },
-	},
-	-- Configure any other settings here. See the documentation for more details.
-	-- colorscheme that will be used when installing plugins.
-	install = { colorscheme = { "" } },
-	-- automatically check for plugin updates
-	checker = { enabled = true },
-	change_detection = {
-		notify = false
-	}
+    defaults = {
+        lazy = true,
+    },
+    spec = {
+        { import = "dan.plugins" },
+    },
+    checker = { enabled = true },
+    change_detection = {
+        notify = false,
+    },
+    performance = {
+        rtp = {
+            -- disable some rtp plugins
+            disabled_plugins = {
+                "gzip",
+                "matchit",
+                "matchparen",
+                "netrwPlugin",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "zipPlugin",
+            },
+        },
+    },
 })
 
 require("dan.helpers.keys").map("n", "<leader>L", lazy.show, "Show Lazy")
--- .config/nvim/lua/dan/plugins/telescope.lua
+++ .config/nvim/lua/dan/plugins/telescope.lua
@@ -2,6 +2,7 @@ local map = require("dan.helpers.keys").map
 
 return {
     "nvim-telescope/telescope.nvim",
+    lazy = false,
     branch = "0.1.x",
     dependencies = {
         "nvim-lua/plenary.nvim",
