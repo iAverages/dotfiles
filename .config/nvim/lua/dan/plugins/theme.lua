4549b4ee977411b61e6014dd2083774a09883c90
iAverages <me@danielraybone.com>
2024-07-31 09:52:17 +0100

    format

--- .config/nvim/lua/dan/plugins/theme.lua
+++ .config/nvim/lua/dan/plugins/theme.lua
@@ -1,12 +1,11 @@
 return {
-  {
-    'sainnhe/sonokai',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- vim.g.sonokai_style = 'shusia'
-      vim.cmd.colorscheme('sonokai')
-    end
-  },
-
+    {
+        "sainnhe/sonokai",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- vim.g.sonokai_style = 'shusia'
+            vim.cmd.colorscheme("sonokai")
+        end,
+    },
 }
