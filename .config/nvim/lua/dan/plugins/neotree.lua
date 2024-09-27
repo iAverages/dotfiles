563943365b190e91b94b7a2dee66d377989963d6
iAverages <me@danielraybone.com>
2024-09-27 09:48:13 +0100

    Merge "uh" nvim config files

--- .config/nvim/lua/dan/plugins/neotree.lua
+++ .config/nvim/lua/dan/plugins/neotree.lua
@@ -7,23 +7,25 @@ return {
         "MunifTanjim/nui.nvim",
         "3rd/image.nvim",
     },
-    opts = {
-        buffers = {
-            follow_currnet_file = {
-                enable = true,
-            },
-        },
-        filesystem = {
-            filtered_items = {
-                hide_dotfiles = false,
-                hide_gitignore = false,
-                hide_by_name = {
-                    "node_modules",
-                    ".git",
-                    "target",
-                    "vendor",
+    config = function()
+        require("transparent").clear_prefix("NeoTree")
+        require("neo-tree").setup({
+            close_if_last_window = true,
+            filesystem = {
+                follow_current_file = {
+                    enabled = true,
+                },
+                filtered_items = {
+                    hide_dotfiles = false,
+                    hide_gitignore = false,
+                    hide_by_name = {
+                        "node_modules",
+                        ".git",
+                        "target",
+                        "vendor",
+                    },
                 },
             },
-        },
-    },
+        })
+    end,
 }
