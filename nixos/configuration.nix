1ab07704bacbd72700a8b179fbd5e03652090cba
iAverages <me@danielraybone.com>
2024-09-30 00:38:41 +0100

    izanami: 114 current  2024-09-30 00:38:36  24.11.20240824.d0e1602  6.10.6                                  *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,8 +1,9 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{...}: {
+{inputs, ...}: {
   imports = [
+    inputs.home-manager.nixosModules.home-manager
     # Include the results of the hardware scan.
     ./hardware.nix
     ./packages.nix
@@ -11,4 +12,11 @@
     ./user.nix
     ./test.nix
   ];
+
+  home-manager = {
+    extraSpecialArgs = {inherit inputs;};
+    users = {
+      dan = import ./home.nix;
+    };
+  };
 }
