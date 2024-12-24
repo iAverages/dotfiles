5fbb37b77b3b26cb870dfc6f899599acbda4af71
iAverages <me@danielraybone.com>
2024-12-24 18:01:35 +0000

    small cleanup

--- .config/ags/app.ts
+++ .config/ags/app.ts
@@ -1,10 +1,12 @@
-import { App } from "astal";
-import style from "./style.scss";
-import Bar from "./widget/bar";
-
-App.start({
-    css: style,
-    main: () => {
-        App.get_monitors().map(Bar);
-    },
-});
+// import { App } from "astal";
+// import style from "./style.scss";
+// import Bar from "./widget/bar";
+//
+// App.start({
+//     css: style,
+//     main: () => {
+//         App.get_monitors().map(Bar);
+//     },
+// });
+//
+import "./src/index";
