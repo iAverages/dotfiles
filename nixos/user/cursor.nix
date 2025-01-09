d8adf8aec67be1c5063267b64a8fb1dde334fcc3
iAverages <me@danielraybone.com>
2025-01-09 20:24:16 +0000

    trying to fix cursor on x & fix ghostty font

--- nixos/user/cursor.nix
+++ nixos/user/cursor.nix
@@ -4,19 +4,7 @@
   ];
 
   home.sessionVariables = {
-    XCURSOR_THEME = "rose-pine";
+    XCURSOR_THEME = "BreezeX-RosePine-Linux";
     XCURSOR_SIZE = "24";
   };
-
-  home.file.".config/gtk-3.0/settings.ini".text = ''
-    [Settings]
-    gtk-cursor-theme-name = rose-pine
-    gtk-cursor-theme-size = 24
-  '';
-
-  home.file.".config/gtk-4.0/settings.ini".text = ''
-    [Settings]
-    gtk-cursor-theme-name = rose-pine
-    gtk-cursor-theme-size = 24
-  '';
 }
