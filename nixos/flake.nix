4a43d18d9518187ac2b90ce13b831a4053d97753
iAverages <me@danielraybone.com>
2025-01-21 22:00:12 +0000

    update ghosttly & private

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -20,7 +20,7 @@
     };
 
     ghostty = {
-      url = "git+ssh://git@github.com/ghostty-org/ghostty?ref=main";
+      url = "github:ghostty-org/ghostty";
 
       # NOTE: The below 2 lines are only required on nixos-unstable,
       # if you're on stable, they may break your build
