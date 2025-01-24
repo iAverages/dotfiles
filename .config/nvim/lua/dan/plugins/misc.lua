ffc2e9d7f372aceb3dc3e9d0e0bcb56f3b496dcd
iAverages <me@danielraybone.com>
2025-01-24 13:24:10 +0000

    add vim-astro plugin

--- .config/nvim/lua/dan/plugins/misc.lua
+++ .config/nvim/lua/dan/plugins/misc.lua
@@ -49,4 +49,5 @@ return {
     },
     "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
     "tpope/vim-surround", -- Surround stuff with the ys-, cs-, ds- commands
+    "wuelnerdotexe/vim-astro",
 }
