a536c24f921ba9608d5987cdee3b93dd4dd28d6a
iAverages <me@danielraybone.com>
2025-04-24 13:58:02 +0100

    actually add fonts

--- nixos/system/wm/fonts.nix
+++ nixos/system/wm/fonts.nix
@@ -1,5 +1,18 @@
 {pkgs, ...}: {
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-  ];
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+      inter
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+    ];
+
+    fontconfig.defaultFonts = {
+      serif = ["Noto Serif" "Noto Serif CJK SC"];
+      sansSerif = ["Inter" "Noto Sans" "Noto Sans CJK SC"];
+      monospace = ["JetBrainsMono Nerd Font" "Noto Sans Mono"];
+      emoji = ["Noto Color Emoji"];
+    };
+  };
 }
