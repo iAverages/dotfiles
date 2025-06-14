8f1495ded52b812aea37a60edf9a504693613bda
iAverages <me@danielraybone.com>
2025-06-14 19:56:09 +0100

    dont show unmodified buffers in bufferline

--- .config/nvim/lua/dan/plugins/bufferline.lua
+++ .config/nvim/lua/dan/plugins/bufferline.lua
@@ -7,11 +7,23 @@ return {
             { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
         },
         opts = {
+
             options = {
                 diagnostics = "nvim_lsp",
                 truncate_names = true,
                 show_buffer_close_icons = false,
                 show_close_icon = false,
+                custom_filter = function(bufnr)
+                    if vim.bo[bufnr].modified then
+                        return true
+                    end
+
+                    if bufnr == vim.api.nvim_get_current_buf() then
+                        return true
+                    end
+
+                    return false
+                end,
             },
         },
     },
