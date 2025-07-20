197d8aba62a55287e24cad91d46ba0725a7a2170
iAverages <me@danielraybone.com>
2025-07-20 17:55:39 +0100

    use ssh url again for private

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
       url = "github:iaverages/mirai";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    private.url = "github:iaverages/nixos-private";
+    private.url = "git+ssh://git@github.com/iAverages/nixos-private?ref=main";
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "nixpkgs/nixos-25.05";
