82d9ab945a0f6c722460dc012e39490cd8500593
iAverages <me@danielraybone.com>
2025-07-20 13:22:27 +0100

    add experimental bluetotth settings

--- nixos/system/hardware/bluetooth.nix
+++ nixos/system/hardware/bluetooth.nix
@@ -1,5 +1,10 @@
 {...}: {
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
+  hardware = {
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+      settings = {General = {Experimental = "true";};};
+    };
+  };
   services.blueman.enable = true;
 }
