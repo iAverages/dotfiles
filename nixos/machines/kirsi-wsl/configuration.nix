00d03b8f144588446360c563e18883e767b40d3f
iAverages <me@danielraybone.com>
2025-10-24 23:26:13 +0100

    change default user

--- nixos/machines/kirsi-wsl/configuration.nix
+++ nixos/machines/kirsi-wsl/configuration.nix
@@ -1,21 +1,13 @@
-{
-  pkgs,
-  config,
-  inputs,
-  ...
-}: {
+{inputs, ...}: {
   imports = [
     inputs.nixos-wsl.nixosModules.default
   ];
-  # environment.terminal.program = "alacritty";
 
   wsl.enable = true;
+  wsl.defaultUser = "dan";
 
   networking.hostName = "kirsi-wsl";
   nixpkgs.hostPlatform = "x86_64-linux";
-
-  services.openssh.enable = true;
-  services.openssh.settings.PermitRootLogin = "no";
 
   boot.tmp.useTmpfs = true;
 }
