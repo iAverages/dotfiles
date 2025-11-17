622fe89f9e1414b13f99ecb6cc39495624559538
iAverages <me@danielraybone.com>
2025-11-17 10:52:27 +0000

    change dns server ip

--- nixos/system/network/dns.nix
+++ nixos/system/network/dns.nix
@@ -2,7 +2,7 @@
   # fix: for whatever reason my ips dns is being included in config, first
   environment.etc = {
     # "resolv.conf".text = "nameserver 192.168.1.11\n";
-    "resolv.conf".text = "nameserver 192.168.1.11\nnameserver 1.1.1.1\n";
+    "resolv.conf".text = "nameserver 192.168.1.12\nnameserver 1.1.1.1\n";
   };
 
   networking.hosts = {
